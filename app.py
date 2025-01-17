4f2b4560bc040939259cdd214a8f3daf74c7d569
KerubinDev <kelvin.moraes117@gmail.com>
2025-01-17 19:24:20 -0300

    inicialização

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask
 from database import configurar_banco
-from routes import app as routes_app
 from flask_login import LoginManager
+import routes
 
 def criar_app():
     """Cria e configura a aplicação Flask"""
     app = Flask(__name__)
     
-    # Configura o banco de dados
-    configurar_banco(app)
+    # Configurações
+    app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurante.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    # Registra as rotas
-    app.register_blueprint(routes_app)
+    # Inicializa o banco de dados
+    configurar_banco(app)
     
     # Configura o Login Manager
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'login'
+    
+    # Registra as rotas
+    routes.init_app(app)
     
     return app
 
